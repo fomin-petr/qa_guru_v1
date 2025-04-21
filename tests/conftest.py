7976cf3274b7bda973c599fa1a0ec65e47add772
Petr Fomin <puffik7@gmail.com>
2025-04-22 01:04:49 +0300

    initial task

---
+++ tests/conftest.py
@@ -0,0 +1,23 @@
+import os
+
+import dotenv
+import pytest
+from http import HTTPStatus
+import requests
+
+
+@pytest.fixture(autouse=True)
+def envs():
+    dotenv.load_dotenv()
+
+
+@pytest.fixture
+def app_url():
+    return os.getenv("APP_URL")
+
+
+@pytest.fixture
+def users(app_url):
+    response = requests.get(f"{app_url}/api/users/")
+    assert response.status_code == HTTPStatus.OK
+    return response.json()
